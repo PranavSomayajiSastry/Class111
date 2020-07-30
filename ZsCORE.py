4e3ea85d257eefc616b31055d2f482e6cc19ffc7
PranavSomayajiSastry <58330938+PranavSomayajiSastry@users.noreply.github.com>
2020-07-30 18:04:53 +0530

    Add files via upload

--- ZsCORE.py
+++ ZsCORE.py
@@ -30,6 +30,62 @@ i_std_start, i_std_end = mean - standDev, mean + standDev
 ii_std_start, ii_std_end = mean - (standDev*2), mean + (standDev*2)
 iii_std_start, iii_std_end = mean - (standDev*3), mean + (standDev*3)
 
-print("Std_1: ", i_std_start, i_std_end)
-print("Std_2: ", ii_std_start, ii_std_end)
-print("Std_3: ", iii_std_start, iii_std_end)
+print("Std_1: ", i_std_start,"and", i_std_end)
+print("Std_2: ", ii_std_start,"and", ii_std_end)
+print("Std_3: ", iii_std_start,"and", iii_std_end)
+
+
+df = pd.read_csv("School_1_Sample.csv")
+data = df["Math_score"].to_list()
+meanOfSamp_1= statistics.mean(data)
+print("The Mean of the Sample 1: ", meanOfSamp_1)
+
+fig = ff.create_distplot([mean_list],["Student Marks"],show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17],mode="lines", name="Mean"))
+fig.add_trace(go.Scatter(x=[meanOfSamp_1, meanOfSamp_1], y=[0, 0.17],mode="lines", name="Mean of Student who had Math Labs"))
+fig.add_trace(go.Scatter(x=[i_std_end, i_std_end], y=[0, 0.17],mode="lines", name="Standard Deviation 1 END"))
+fig.add_trace(go.Scatter(x=[ii_std_end, ii_std_end], y=[0, 0.17],mode="lines", name="Standard Deviation 2 END"))
+fig.add_trace(go.Scatter(x=[iii_std_end, iii_std_end], y=[0, 0.17],mode="lines", name="Standard Deviation 3 END"))
+
+fig.show()
+
+
+
+df = pd.read_csv("School_2_Sample.csv")
+data = df["Math_score"].to_list()
+meanOfSamp_2= statistics.mean(data)
+print("The Mean of the Sample 2: ", meanOfSamp_2)
+
+fig = ff.create_distplot([mean_list],["Student Marks"],show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17],mode="lines", name="Mean"))
+fig.add_trace(go.Scatter(x=[meanOfSamp_2, meanOfSamp_2], y=[0, 0.17],mode="lines", name="Mean of Students who used Application"))
+fig.add_trace(go.Scatter(x=[i_std_end, i_std_end], y=[0, 0.17],mode="lines", name="Standard Deviation 1 END"))
+fig.add_trace(go.Scatter(x=[ii_std_end, ii_std_end], y=[0, 0.17],mode="lines", name="Standard Deviation 2 END"))
+fig.add_trace(go.Scatter(x=[iii_std_end, iii_std_end], y=[0, 0.17],mode="lines", name="Standard Deviation 3 END"))
+
+fig.show()
+
+
+
+df = pd.read_csv("School_3_Sample.csv")
+data = df["Math_score"].to_list()
+meanOfSamp_3= statistics.mean(data)
+print("The Mean of the Sample 3: ", meanOfSamp_3)
+
+fig = ff.create_distplot([mean_list],["Student Marks"],show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17],mode="lines", name="Mean"))
+fig.add_trace(go.Scatter(x=[meanOfSamp_3, meanOfSamp_3], y=[0, 0.17],mode="lines", name="Mean of Students who Math Registers"))
+fig.add_trace(go.Scatter(x=[i_std_end, i_std_end], y=[0, 0.17],mode="lines", name="Standard Deviation 1 END"))
+fig.add_trace(go.Scatter(x=[ii_std_end, ii_std_end], y=[0, 0.17],mode="lines", name="Standard Deviation 2 END"))
+fig.add_trace(go.Scatter(x=[iii_std_end, iii_std_end], y=[0, 0.17],mode="lines", name="Standard Deviation 3 END"))
+
+fig.show()
+
+z_score= (meanOfSamp_1-mean )/standDev
+print("The Z-Score of Sample 1:", z_score)
+
+z_score2= (meanOfSamp_2-mean)/standDev
+print("The Z-Score of Sample 2:", z_score2)
+
+z_score3= (meanOfSamp_3-mean )/standDev
+print("The Z-Score of Sample 3:", z_score3)
